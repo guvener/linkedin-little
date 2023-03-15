9c31b37af3263886f811def4f0e4e796c4da1bc7
Guvener Gokce <guvener@guvenergokce.com>
2023-03-15 08:19:58 +0100

    init

---
+++ LinkedinLittle.js
@@ -0,0 +1,68 @@
+// LinkedIn little Text Format Parser
+// readme: https://github.com/guvener/linkedin-little
+class LinkedinLittle {
+
+    /*
+       @test: findHashtags(`Haltet euch fest, oder lasst euch durch uns unterstützen.\n\n{hashtag|\\#|gartenbau} {hashtag|\\#|menzimuck} {hashtag|\\#|zürichoberland}`);
+       @returns:  [ "gartenbau", "menzimuck", "zürichoberland" ]
+    */
+    findHashtags(str) {
+        let results = [];
+        if(!str) return results;
+
+        const regex = /(?<={hashtag\|\\#\|)[^}]+/gm;
+        // Alternative syntax using RegExp constructor
+        let m;
+        while ((m = regex.exec(str)) !== null) {
+            // This is necessary to avoid infinite loops with zero-width matches
+            if (m.index === regex.lastIndex) {
+                regex.lastIndex++;
+            }
+
+            // The result can be accessed through the `m`-variable.
+            m.forEach((match, groupIndex) => {
+                results.push(match);
+            });
+        }
+        return results;
+    }
+
+    /*
+        @test: findMentions("Hello @[Acme Corp](urn:li:organization:1234)")
+        @returns:
+        [
+            {
+                "matched": "@[Acme Corp](urn:li:organization:1234)",
+                "mention": "Acme Corp",
+                "urn": "urn:li:organization:1234",
+                "type": "organization"
+            }
+        ]
+     */
+    findMentions(str) {
+        let results = [];
+        if(!str) return results;
+
+        const regex = /@\[([^\]]+)\]\((urn:li:(person|organization):\d+)\)/gm;
+        // Alternative syntax using RegExp constructor
+
+        let m;
+        while ((m = regex.exec(str)) !== null) {
+            // This is necessary to avoid infinite loops with zero-width matches
+            if (m.index === regex.lastIndex) {
+                regex.lastIndex++;
+            }
+
+            let element = {};
+            let groups = ['matched', 'mention', 'urn', 'type'];
+            // The result can be accessed through the `m`-variable.
+            m.forEach((match, groupIndex) => {
+                if(groupIndex < groups.length) element[groups[groupIndex]] = match;
+            });
+            results.push(element);
+        }
+        return results;
+    }
+}
+
+
--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# linkedin-little
-LinkedIn little Text Format Parser Class JS
+# LinkedIn little Text Format Parser JS
+Simple LinkedIn little Text Format Parser in JS
+[LinkedIn little Documentation](https://learn.microsoft.com/en-us/linkedin/marketing/integrations/community-management/shares/little-text-format?view=li-lms-2023-02)
+
+```js
+(new LinkedinLittle).findHashtags(`💪Haltet euch fest, oder lasst euch durch uns unterstützen.\n\n{hashtag|\\#|gartenbau} {hashtag|\\#|menzimuck} {hashtag|\\#|zürichoberland} {hashtag|\\#|altwegggartenbau}`);
+// @returns:  [ "gartenbau", "menzimuck", "zürichoberland", "altwegggartenbau" ]
+```
+
+```js
+(new LinkedinLittle).findMentions("Hello @[Acme Corp](urn:li:organization:1234)")
+/* @returns:
+        [
+            {
+                "matched": "@[Acme Corp](urn:li:organization:1234)",
+                "mention": "Acme Corp",
+                "urn": "urn:li:organization:1234",
+                "type": "organization"
+            }
+        ]
+*/
+```
